c2ba4539dc7187c1837078c8ecc47c3b75f11937
nitokrisalpha <nitokrisalpha@outlook.com>
2025-10-01 13:55:08 +0800

    添加路径初始化

--- service/src/main/kotlin/com/nitokrisalpha/infranstructure/config/PathConfig.kt
+++ service/src/main/kotlin/com/nitokrisalpha/infranstructure/config/PathConfig.kt
@@ -2,6 +2,7 @@ package com.nitokrisalpha.infranstructure.config
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
+import kotlin.io.path.Path
 
 @Component
 @ConfigurationProperties("nhentai.path")
@@ -11,4 +12,10 @@ class PathConfig(
     val galleries: String = "/app/static/galleries",
     val files: String = "/app/static/files"
 ) {
+    init {
+        Path(tmpPath).toFile().mkdirs()
+        Path(previews).toFile().mkdirs()
+        Path(galleries).toFile().mkdirs()
+        Path(files).toFile().mkdirs()
+    }
 }

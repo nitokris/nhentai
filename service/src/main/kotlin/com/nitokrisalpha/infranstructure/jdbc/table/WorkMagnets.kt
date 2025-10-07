0448da571835494e5ae7cc637c856810a74b5308
nitokrisalpha <nitokrisalpha@outlook.com>
2025-10-07 22:25:01 +0800

    调整表结构

--- service/src/main/kotlin/com/nitokrisalpha/infranstructure/jdbc/table/WorkMagnets.kt
+++ service/src/main/kotlin/com/nitokrisalpha/infranstructure/jdbc/table/WorkMagnets.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.v1.core.dao.id.LongIdTable
 
 object WorkMagnets : LongIdTable() {
     val workId = char("work_id", 32).references(ref = Works.businessId, onDelete = ReferenceOption.CASCADE)
-    val magnetId = char("magnet_id", 32).references(ref = Magnets.businessId, onDelete = ReferenceOption.CASCADE)
+    val magnetId = char("magnet_id", 40).references(ref = Magnets.businessId, onDelete = ReferenceOption.CASCADE)
 
     init {
         uniqueIndex(customIndexName = "work_magnets", workId, magnetId)
